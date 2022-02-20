64edff61726686107441eb0f8ceab346681a524e
KomiSans <sansdavin8@gmail.com>
2022-02-20 10:55:27 +0800

    feature-Config类以及Util类添加

---
+++ src/Casdoor.Client/Config/CasdoorConfig.cs
@@ -0,0 +1,36 @@
+﻿namespace Casdoor.Client.Config
+{
+    /// <summary>
+    ///  Casdoor 配置类
+    /// </summary>
+    [Serializable]
+    public class CasdoorConfig
+    {
+
+        public CasdoorConfig(string _endPoint, string _clientId, string _clientSecret, string _jwtPublicKey, string _orginazationName)
+        {
+            endPoint = _endPoint
+                ?? throw new ArgumentNullException(nameof(endPoint));
+            clientId = _clientId
+                ?? throw new ArgumentNullException(nameof(clientId));
+            clientSecret = _clientSecret
+                ?? throw new ArgumentNullException(nameof(clientSecret));
+            jwtPublicKey = _jwtPublicKey
+                ?? throw new ArgumentNullException(nameof(jwtPublicKey));
+            orginazationName = _orginazationName
+                ?? throw new ArgumentNullException(nameof(orginazationName));
+        }
+
+        private string endPoint { get; set; }
+
+        private string clientId { get; set; }
+
+        private string clientSecret { get; set; }
+
+        private string jwtPublicKey { get; set; }
+
+        private string orginazationName { get; set; }
+
+    }
+
+}
---
+++ src/Casdoor.Client/Utils/http/CasdoorResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace Casdoor.Client.Utils.http
+{
+    [Serializable]
+    public class CasdoorResponse
+    {
+        private string? status { get; set; }
+
+        private string? data { get; set; }
+
+        private string? msg { get; set; }
+
+        private string? data2 { get; set; }
+    }
+}
