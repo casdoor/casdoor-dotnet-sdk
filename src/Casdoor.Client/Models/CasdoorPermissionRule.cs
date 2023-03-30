7d8acf84990ad632fc5a522defa8bf8a1bb653e9
isulimanov <Iliya.Sulimanov@live.ru>
2023-03-30 09:34:44 +0300

    feat: add support enforce API

---
+++ src/Casdoor.Client/Models/CasdoorPermissionRule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Casdoor.Client;
+
+public class CasdoorPermissionRule
+{
+    public string? Ptype { get; set; }
+    public string? V0 { get; set; }
+    public string? V1 { get; set; }
+    public string? V2 { get; set; }
+    public string? V3 { get; set; }
+    public string? V4 { get; set; }
+    public string? V5 { get; set; }
+    public string? Id { get; set; }
+}
+
+
