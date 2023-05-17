03f57c12a4d746424dde76d3dd7686c015af300d
Sergey Zabodalov <zabbius@gmail.com>
2023-05-17 19:42:27 +0300

    feat: API for applications, organizations and providers

---
+++ src/Casdoor.Client/CasdoorClient.OrganizationApi.cs
@@ -0,0 +1,62 @@
+// Copyright 2022 The Casdoor Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Net.Http.Json;
+
+namespace Casdoor.Client;
+
+public partial class CasdoorClient
+{
+    public virtual async Task<CasdoorResponse?> AddOrganizationAsync(CasdoorOrganization organization)
+    {
+        if (string.IsNullOrEmpty(organization.Owner))
+        {
+            organization.Owner = CasdoorConstants.DefaultCasdoorOwner;
+        }
+        var url = _options.GetActionUrl("add-organization");
+        return await PostAsJsonAsync(url, organization);
+    }
+
+    public virtual async Task<CasdoorResponse?> DeleteOrganizationAsync(string name)
+    {
+        var organization = new CasdoorOrganization {Owner = CasdoorConstants.DefaultCasdoorOwner, Name = name};
+        var url = _options.GetActionUrl("delete-organization");
+        return await PostAsJsonAsync(url, organization);
+    }
+
+    public virtual async Task<CasdoorResponse?> UpdateOrganizationAsync(string id, CasdoorOrganization newOrganization)
+    {
+        var queryMap = new QueryMapBuilder().Add("id", id).GetMap();
+        if (string.IsNullOrEmpty(newOrganization.Owner))
+        {
+            newOrganization.Owner = CasdoorConstants.DefaultCasdoorOwner;
+        }
+        var url = _options.GetActionUrl("update-organization", queryMap);
+        return await PostAsJsonAsync(url, newOrganization);
+    }
+
+    public virtual Task<CasdoorOrganization?> GetOrganizationAsync(string id)
+    {
+        var queryMap = new QueryMapBuilder().Add("id", id).GetMap();
+        var url = _options.GetActionUrl("get-organization", queryMap);
+        return _httpClient.GetFromJsonAsync<CasdoorOrganization>(url);
+    }
+
+    public virtual Task<IEnumerable<CasdoorOrganization>?> GetOrganizationsAsync(string owner)
+    {
+        var queryMap = new QueryMapBuilder().Add("owner", owner).GetMap();
+        var url = _options.GetActionUrl("get-organizations", queryMap);
+        return _httpClient.GetFromJsonAsync<IEnumerable<CasdoorOrganization>>(url);
+    }
+}
---
+++ src/Casdoor.Client/Models/CasdoorOrganization.cs
@@ -0,0 +1,84 @@
+// Copyright 2022 The Casdoor Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Text.Json.Serialization;
+
+namespace Casdoor.Client;
+
+
+public class CasdoorAccountItem
+{
+    [JsonPropertyName("name")]
+    public string? Name { get; set; }
+
+    [JsonPropertyName("visibles")]
+    public bool? Visible { get; set; }
+
+    [JsonPropertyName("viewRule")]
+    public string? ViewRule { get; set; }
+
+    [JsonPropertyName("modifyRule")]
+    public string? ModifyRule { get; set; }
+}
+
+public class CasdoorOrganization
+{
+    [JsonPropertyName("owner")]
+    public string? Owner { get; set; }
+
+    [JsonPropertyName("name")]
+    public string? Name { get; set; }
+
+    [JsonPropertyName("createdTime")]
+    public string? CreatedTime { get; set; }
+
+    [JsonPropertyName("displayName")]
+    public string? DisplayName { get; set; }
+
+    [JsonPropertyName("websiteUrl")]
+    public string? WebsiteUrl { get; set; }
+
+    [JsonPropertyName("favicon")]
+    public string? Favicon { get; set; }
+
+    [JsonPropertyName("passwordType")]
+    public string? PasswordType { get; set; }
+
+    [JsonPropertyName("passwordSalt")]
+    public string? PasswordSalt { get; set; }
+
+    [JsonPropertyName("phonePrefix")]
+    public string? PhonePrefix { get; set; }
+
+    [JsonPropertyName("defaultAvatar")]
+    public string? DefaultAvatar { get; set; }
+
+    [JsonPropertyName("defaultApplication")]
+    public string? DefaultApplication { get; set; }
+
+    [JsonPropertyName("tags")]
+    public string[]? Tags { get; set; }
+
+    [JsonPropertyName("masterPassword")]
+    public string? MasterPassword { get; set; }
+
+    [JsonPropertyName("enableSoftDeletion")]
+    public bool? EnableSoftDeletion { get; set; }
+
+    [JsonPropertyName("isProfilePublic")]
+    public bool? IsProfilePublic { get; set; }
+    
+    [JsonPropertyName("accountItems")]
+    CasdoorAccountItem[]? AccountItems { get; set; }
+}
